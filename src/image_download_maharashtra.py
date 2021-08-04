85ab1395db8131c1adaf609f140855b69f1f17f7
shubhamsharma1609 <shubham.thb@gmail.com>
2021-08-04 19:42:48 +0530

    added coveralls

--- src/image_download_maharashtra.py
+++ src/image_download_maharashtra.py
@@ -14,13 +14,14 @@ import cv2
 import multiprocessing as mp
 
 # Mapbox access token
-MAT=''
+MAT=os.getenv('MAT')
 
 # Function for downloading mapbox raster tile
 def mapbox_download(tile, city):
     z = 17 # Zoom level
     
     outpath='../inference/images/'+city+'/'
+    stat_list=[]
     if not os.path.exists(outpath):
         os.makedirs(outpath)
     try:
@@ -30,25 +31,35 @@ def mapbox_download(tile, city):
             image = np.asarray(bytearray(r.raw.read()), dtype="uint8")
             image = cv2.imdecode(image, cv2.IMREAD_COLOR)
             cv2.imwrite(outpath +str(tile[2])+'.'+str(tile[0]) + '.' + str(tile[1]) + '.png', image)
+            stat_list.append(200)
     except:
         print('Crashed while {0} in {1},{2}'.format(city, tile[0], tile[1]))   
     
+
+    return stat_list
+    
+    
+    
 if __name__ == '__main__':
-    csv_list = [f for f in os.listdir('../inference/infra_info/') if f.endswith('.csv')]
+    csv_list = [f for f in os.listdir('../inference/infra_info/test_csv/') if f.endswith('.csv')]
     with tqdm(total = len(csv_list), desc = 'No. of cities') as pbar1:
         for city in csv_list:
-            df = pd.read_csv('./infra_info/{}'.format(city))
+            df = pd.read_csv('./infra_info/test_csv/{}'.format(city))
             df_infra = df[df['infra'] == True].reset_index().drop('index', axis = 1)
             xtile = df_infra['xtile'].to_list()
             ytile = df_infra['ytile'].to_list()
             count = df_infra.index.to_list()
-            # Parallelizing the image download with multiple processes
+            #assert mapbox_download(test,MAT)==[200]*test.shape[0]
+            ############For parallelization###########
+            Parallelizing the image download with multiple processes
             with mp.Pool(mp.cpu_count() - 11) as pool:
                 with tqdm(total = df_infra.shape[0], desc = '{} Progress'.format(os.path.splitext(city)[0])) as pbar2:
                     temp = partial(mapbox_download, city = os.path.splitext(city)[0])
+                    print(temp)
                     for i, _ in enumerate(pool.imap_unordered(temp, iterable = zip(xtile, ytile, count))):
                         pbar2.update()
-            os.rename('../inference/infra_info/{}'.format(city), '../inference/infra_info/downloaded/{}'.format(city))
+            #os.rename('../inference/infra_info/test_csv/{}'.format(city), '../inference/infra_info/test_csv/downloaded/{}'.format(city))
+            assert len(os.listdir('../inference/images/'+city[:-4]+'\\'))==len(df_infra)
             pbar1.update(1)
 
 
